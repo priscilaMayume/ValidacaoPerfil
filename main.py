a060a593f76ac8c79f56ee9e7fc5cd1c1c618bac
prisccila <>
2021-04-19 11:30:42 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,37 @@
+#REGRAS
+
+#Nome: maior que 3 caracteres;
+#Idade: entre 0 e 150;
+#Salário: maior que zero;
+#Sexo: 'f' ou 'm';
+#Estado Civil: 's', 'c', 'v', 'd';
+
+#*********************#
+
+nome=str(input("informe um nome--> "))
+while ( len(nome) <=  3 ):
+	nome=str(input("informe um nome--> "))
+
+#Idade: entre 0 e 150;
+
+idade=int(input("informe a idade--> "))
+while ( idade > 150 or idade < 0 ):
+	idade=int(input("informe a idade--> "))
+	
+	
+#Salário: maior que zero;
+salario=float(input("informe um salário--> "))
+while ( salario < 0 ):
+	salario=float(input("informe um salário--> "))
+	
+#Sexo: 'f' ou 'm';
+
+sexo=str(input("informe a inicial do seu sexo--> "))
+while  sexo !="f" and sexo!="m" :
+	sexo=str(input("informe a inicial do seu sexo--> "))
+	
+#Estado Civil: 's', 'c', 'v', 'd';
+
+estado_civil=str(input("informe a inicial do seu estado civil-->"))
+while ( estado_civil != "s" and estado_civil != "c" and estado_civil != "v" and estado_civil != "d" ):
+	estado_civil=str(input("informe a inicial do seu estado civil-->"))
